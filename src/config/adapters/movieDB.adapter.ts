882ac6602e40840352b7ced27ca5d95e44cb25ab
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-03-19 07:49:57 -0500

    eliminando

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -4,7 +4,7 @@ import { AxiosAdapter } from "./http/axios.adapters";
 export const movieDBFetcher = new AxiosAdapter({
     baseUtl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'd5e3b6f4b4d8c1b3c7f9d3e7a9e7b1f0',
+        api_key: '',
         language: 'es'
     }
 })
